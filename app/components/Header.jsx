e49063809225773f2709075340c1db6b97141cc4
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-01-15 16:36:43 +0300

    Enhance About and Header components with motion effects for improved user experience; add viewport settings for animations

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,6 +10,7 @@ const Header = () => {
         initial={{ scale: 0 }}
         whileInView={{ scale: 1 }}
         transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+        viewport={{ once: true, amount: 0.2 }}
       >
         <Image src={assets.my_pic} alt="" className="rounded-full w-32" />
       </motion.div>
@@ -17,6 +18,7 @@ const Header = () => {
         initial={{ y: -20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.6, delay: 0.3 }}
+        viewport={{ once: true, amount: 0.2 }}
         className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo"
       >
         Hi, I'm Ahmad Alidlbi{" "}
@@ -26,6 +28,7 @@ const Header = () => {
         initial={{ y: -30, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.5 }}
+        viewport={{ once: true, amount: 0.2 }}
         className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo"
       >
         Software Engineer based in Kuwait
@@ -34,6 +37,7 @@ const Header = () => {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.6, delay: 0.7 }}
+        viewport={{ once: true, amount: 0.2 }}
         className="max-w-2xl mx-auto font-Ovo"
       >
         I'm a passionate developer with a love for creating innovative solutions
@@ -47,6 +51,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1 }}
+          viewport={{ once: true, amount: 0.2 }}
           href="#contact"
           className="px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2 dark:bg-transparent"
         >
@@ -58,6 +63,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
+          viewport={{ once: true, amount: 0.2 }}
           href="/sample-resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
