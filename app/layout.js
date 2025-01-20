c8551185bd902315a68ace8843dd8f513e52a5f6
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-01-20 22:05:33 +0300

    Update portfolio image URL in layout.js

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
     url: "https://ahmadalidlbi.com",
     images: [
       {
-        url: "https://www.ahmadalidlbi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fme.86a19511.png&w=3840&q=75",
+        url: "https://www.ahmadalidlbi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmy_pic.e8a84c6d.png&w=3840&q=75",
         width: 1200,
         height: 630,
         alt: "Ahmad Alidlbi's portfolio image",
