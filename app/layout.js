5ac9f7b7469182283aaa7b49eb09526e01804fff
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-01-22 16:30:13 +0300

    Add sitemap and robots.txt for improved SEO; update site URL in metadata

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
     title: "Ahmad Alidlbi | Software Engineer",
     description:
       "I’m a Software Engineer based in Kuwait, passionate about crafting software solutions. I studied Software Engineering at Altınbaş University in Istanbul, where I built a strong foundation in software principles. With expertise in React, React Native, I focus on delivering scalable, user-friendly, and impactful digital solutions. My experience also includes tools like MongoDB, Firebase, Node.js, and Figma, and I’m driven by a constant desire to learn and adapt to emerging technologies.",
-    url: "https://ahmadalidlbi.com",
+    url: "https://www.ahmadalidlbi.com",
     images: [
       {
         url: "https://www.ahmadalidlbi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmy_pic.e8a84c6d.png&w=3840&q=75",
@@ -28,6 +28,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="canonical" href="https://www.ahmadalidlbi.com" />
+      </head>
       <body
         className={`${poppins.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.ahmadalidlbi.com', // Your preferred domain
+    generateRobotsTxt: true, // Optionally generate a robots.txt file
+    changefreq: 'daily',
+    priority: 0.7,
+  };
