95a75f818466d0352da6e7dfbb2132c96298a61f
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-01-22 19:20:19 +0300

    Fix syntax error in layout.js by correcting array formatting for social links

--- app/layout.js
+++ app/layout.js
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
               "https://www.linkedin.com/in/ahmadalidlbi",
               "https://github.com/ahmadalidlbi",
               "https://www.instagram.com/dev0ex",
-            ],
+            ]
             "jobTitle": "Software Engineer",
             "description": "I'm Ahmad Alidlbi, a Software Engineer based in Kuwait, passionate about crafting software solutions.",
             "image": "https://www.ahmadalidlbi.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmy_pic.e8a84c6d.png&w=3840&q=75"
